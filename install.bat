385bd3f579c0c22b5f5c5104ec67f78720089747
isaac <awesomeisaac2000@gmail.com>
2019-03-16 15:40:42 -0600

    Finished writing batch script for installing on windows

--- install.bat
+++ install.bat
@@ -1,10 +1,40 @@
-@echo off
+@echo off 
+setlocal ENABLEDELAYEDEXPANSION
 
-set pos_paths[0] = "%HOMEDRIVE%%HOMEPATH%/Arduino15/preferences.txt"
-set pos_paths[1] = "%HOMEDRIVE%%HOMEPATH%/Arduino15/preferences.txt"
-set pos_paths[2] = "%HOMEDRIVE%%HOMEPATH%/Arduino/preferences.txt"
-set pos_paths[3] = "%HOMEDRIVE%%HOMEPATH%\Documents/ArduinoData/preferences.txt"
+REM Possible Root paths where arduino placed it's preferences
+set "ROOT_PATHS=(%LOCALAPPDATA% %APPDATA% %HOMEDRIVE%%HOMEPATH% )"
 
-(for %%pos_path in (%pos_paths%) do ( 
-   echo %%pos_path 
-))
+REM All possible locations of the arduino config file given the above roots
+set "FILE_PATHS=(\Arduino15\preferences.txt \Arduino\preferences.txt \Documents\ArduinoData\preferences.txt)"
+
+REM Iterate the possible root paths
+for %%r in %ROOT_PATHS% do (
+
+	REM Iterate all possible locations of arduino file within the root locations
+	for %%f in %FILE_PATHS% do (
+		echo "%%r%%f"
+	
+		if exist "%%r%%f" (
+			echo Found arduino ide config file '%%r%%f' reading data...
+			
+			REM Get line in config file that says 'sketchbook.path', which is the path to the sketchbook
+			for /f "tokens=1 delims=" %%i in ('findstr "sketchbook.path" "%%r%%f"') do (
+				REM Get everything after the equal sign to just get the file path.
+				for /f "tokens=2 delims==" %%a in ("%%i") do (
+					REM Perform copy, copy examples in this folder over to the arduino sketchbook folder
+					xcopy "%~dp0\MakerSpaceExamples" "%%a\MakerSpaceExamples" /E /Y /I
+					
+					echo _____
+					echo Install of MakerSpaceExamples Complete, Restart Arduino IDE Now to see them.
+					pause
+					exit 0
+				)
+			)
+		)
+	)
+)
+REM We could not find config file for Arduino IDE at any of the typical locations,
+REM let the user know and exit with error code 1
+echo "ERROR: No config file for arduino ide found..."
+pause
+exit 1
